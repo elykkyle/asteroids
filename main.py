ef967450177b56508c3e6278e137f7247260a5ff
Kyle Williams <15004488+elykkyle@users.noreply.github.com>
2025-07-02 20:17:01 -0500

    add groups

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for i in drawable:
+            i.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 
